2a009047f4d9befcb088d9228aa2df9cd6549bc4
pedro <p3drov1n1cyus@gmail.com>
2025-02-17 20:40:00 -0300

    resolvendo CORS

--- src/admin/app/main.py
+++ src/admin/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from .routes import usuario, venda, produto
 from sqlalchemy import create_engine, text
 from .database import Base, engine, settings, database_exists
+from fastapi.middleware.cors import CORSMiddleware
 
 # Cria o banco de dados, se não existir
 def create_database():
@@ -21,6 +22,15 @@ create_database()
 
 # Inicializa o FastAPI
 app = FastAPI()
+
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # Substitua pelo endereço do seu frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
 
 # Rotas das APIs
 app.include_router(usuario.router, prefix="/api", tags=["Usuários"])
